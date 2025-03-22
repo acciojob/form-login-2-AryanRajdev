50848f46ff32e34a09522aeb71e12cd82cacb378
AryanRajdev <91838247+AryanRajdev@users.noreply.github.com>
2025-03-22 22:06:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let myform = document.querySelector("#my-form");
 myform.addEventListener("submit", (event) => {
     event.preventDefault();  // Prevent page reload on form submission
 
-    let firstName = document.querySelector('input[name="FirstName"]').value;
-    let lastName = document.querySelector('input[name="LastName"]').value;
-    let phone = document.querySelector('input[name="PhoneNumber"]').value;
-    let email = document.querySelector('input[name="EmailID"]').value;
+    let firstName = document.querySelector('input[name="First Name"]').value;
+    let lastName = document.querySelector('input[name="Last Name"]').value;
+    let phone = document.querySelector('input[name="Phone Number"]').value;
+    let email = document.querySelector('input[name="Email ID"]').value;
 
     alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phone} Email ID: ${email}`);
 
