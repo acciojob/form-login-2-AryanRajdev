06d17fc14726e43242270d5fd9ae248443b7aab0
AryanRajdev <91838247+AryanRajdev@users.noreply.github.com>
2025-03-27 20:39:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ myform.addEventListener("submit", (event) => {
     alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phone} Email ID: ${email}`);
 
     // Clear input fields after submission
-    document.querySelector('input[name="FirstName"]').value = "";
-    document.querySelector('input[name="LastName"]').value = "";
-    document.querySelector('input[name="PhoneNumber"]').value = "";
-    document.querySelector('input[name="EmailID"]').value = "";
+    document.querySelector('input[name="First Name"]').value = "";
+    document.querySelector('input[name="Last Name"]').value = "";
+    document.querySelector('input[name="Phone Number"]').value = "";
+    document.querySelector('input[name="Email ID"]').value = "";
 });
